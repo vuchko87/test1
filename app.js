c8b535b5e54ae59998c57514253472b1c23e0487
Nemanja Vucic <infopikado@gmail.com>
2025-06-13 14:41:50 +0200

    FirsSite

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Welcome in app foka|Software");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Dobro došli na Nemanjin sajt</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Sajt je pokrenut na http://localhost:${port}`);
+});
